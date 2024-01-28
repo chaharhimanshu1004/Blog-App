8a898862b594f5ae123cacc361de26ef41a3c6e8
himanshu1004200 <chaharhimanshu03@gmail.com>
2024-01-28 19:06:00 +0530

    register route done!

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const cors = require('cors');
+const userRoute = require('./routes/User')
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI).then(()=>{console.log('DataBase is connected !!')}).catch((e)=>{console.log('Error in connecting database')})
+
 
 app.use(cors())
 app.use(express.json());
+
+
 
 
 const PORT = process.env.PORT || 6005
@@ -17,6 +23,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Running !!!');
 });
+app.use('/api/users',userRoute)
 
 
 app.listen(PORT,()=>{
---
+++ server/routes/User.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router();
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const usermodel = require('../modals/User')
+
+require('dotenv').config();
+
+router.post('/register',async(req,res)=>{
+    try{
+        const {username,password} = req.body;
+        const existinguser = await usermodel.findOne({username});
+        if(existinguser){
+            return res.status(400).json({message:"User already Exists"});
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = new usermodel({
+            username,
+            password:hashedPassword
+        })
+        await newUser.save();
+        res.status(200).json({message:"User registered Successfully!!"})
+
+    }catch(err){
+        res.status(500).json({message:"Error in Registering the user! Please try again after some time!"})
+    }
+    
+
+});
+
+router.post('/login',async(req,res)=>{
+    try{
+        const {username,password} = req.body;
+        const userExists = await usermodel.findOne({username});
+        if(!userExists){
+            return res.status(400).json({message:"Error! User doesn't exists!!"})
+        }
+        const isValidPassword = await bcrypt.compare(password,userExists.password);
+        if(!isValidPassword){
+            return res.json({message:"Password doesn't match"})
+        }
+
+    }catch(err){
+        res.status(500).json({message:"Error in Loggin in ! Please try again after some time!"})
+
+        
+    }
+})
+module.exports = router;
+
